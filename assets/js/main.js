1f5a91e463b0607e51d30d8f4108920927d36393
iac31 <iandreicristian@gmail.com>
2018-09-30 00:56:38 +0300

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
-var numbers = document.querySelectorAll('.no');
-var display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.no');
+let display = document.querySelector('.display');
 display.innerHTML = 0;
-var operators = document.querySelectorAll('.btn-op');
-var factorial = document.querySelector('#factorial');
-var sqrt = document.querySelector('#sqrt');
-var power = document.querySelector('#power');
-var clear = document.querySelector('#clear');
-var result = document.querySelector('#result');
+const operators = document.querySelectorAll('.btn-op');
+const factorial = document.querySelector('#factorial');
+const sqrt = document.querySelector('#sqrt');
+const power = document.querySelector('#power');
+const clear = document.querySelector('#clear');
+const result = document.querySelector('#result');
 
-var getDisplayNumber = () => {
+const getDisplayNumber = () => {
     return Number(display.innerHTML);
-}
+};
 
-for (var i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', function(){
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function () {
         if(display.innerHTML === "0") {
             display.innerHTML = '';
         }
@@ -21,40 +21,67 @@ for (var i = 0; i < numbers.length; i++) {
     });
 }
 
-for (i = 0; i < operators.length; i++) {
-    operators[i].addEventListener('click', function (){
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', function() {
         display.innerHTML += this.innerHTML;
     });
 }
 
-var getFactorial = () => {
-    var displayNumber = getDisplayNumber();
-    var factorialResult = 1;
-    for (i = 2; i <= displayNumber; i++) {
+const getFactorial = () => {
+    const displayNumber = getDisplayNumber();
+    let factorialResult = 1;
+    for (let i = 2; i <= displayNumber; i++) {
         factorialResult *= i;
     }
     display.innerHTML = factorialResult;
-}
+};
 
-var getSqrt = () => {
-    var displayNumber = getDisplayNumber();
+const getSqrt = () => {
+    const displayNumber = getDisplayNumber();
     display.innerHTML = Math.sqrt(displayNumber);
-}
+};
 
-var getPower = () => {
+const getPower = () => {
     console.error('NOPE');
-}
+};
 
-var equal = () => {
+const equal = () => {
     display.innerHTML = eval(display.innerHTML);
-}
+};
 
-var clearDisplay = () => {
+const clearDisplay = () => {
     display.innerHTML = 0;
-}
+};
 
-factorial.addEventListener('click', getFactorial);
-sqrt.addEventListener('click', getSqrt);
-power.addEventListener('click', getPower);
-result.addEventListener('click', equal);
-clear.addEventListener('click', clearDisplay);
+const functions = [
+    {
+        name: factorial,
+        func: getFactorial
+    },
+    {
+        name: sqrt,
+        func: getSqrt
+    },
+    {
+        name: power,
+        func: getPower
+    },
+    {
+        name: result,
+        func: equal
+    },
+    {
+        name: clear,
+        func: clearDisplay
+    }
+];
+
+functions.forEach(item => {
+    item.name.addEventListener('click', item.func);
+});
+
+// factorial.addEventListener('click', getFactorial);
+// sqrt.addEventListener('click', getSqrt);
+// power.addEventListener('click', getPower);
+// result.addEventListener('click', equal);
+// clear.addEventListener('click', clearDisplay);
